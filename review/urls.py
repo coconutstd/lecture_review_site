c5794510c13085ac7f8899fba1d004a1ddd83d9a
coconut <wnsspdl@naver.com>
2020-08-12 15:52:17 +0900

    #1 review앱 review_detail과 연결되는 링크구현

--- review/urls.py
+++ review/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('review', views.review_list, name='review_list'),
-    path('review/<int:question_id>/', views.detail, name='review_detail'),
+    path('review/<int:question_id>/', views.review_detail, name='review_detail'),
     path('review/<int:question_id>/results/', views.results, name='review_results'),
     path('review/<int:question_id>/vote/', views.vote, name='review_vote'),
 ]
--- review/views.py
+++ review/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.http import Http404
 from .models import Question
 
 # Create your views here.
@@ -7,8 +8,13 @@ def review_list(request):
     review_lists = Question.objects.order_by('-pub_date')
     return render(request, 'review/review_list.html', {'review_lists': review_lists})
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+def review_detail(request, question_id):
+    review = get_object_or_404(Question, pk=question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    return render(request, 'review/review_detail.html', {'review': review})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
