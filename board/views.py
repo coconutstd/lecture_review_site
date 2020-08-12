1471ec767296e5942e4fa0049b8fbf6433022a40
redcarrot01 <redccc9010@gmail.com>
2020-08-12 17:46:58 +0900

    #2 보드 중간점검3 url 문제 해결

--- board/views.py
+++ board/views.py
@@ -4,30 +4,30 @@ from .models import Post_board, Comment
 from .forms import PostModelForm, PostForm, CommentModelForm
 # Create your views here.
 
-def add_comment_to_post(request, pk):
-    post = get_object_or_404(Post_board, pk=pk)
-    if request.method =='POST':
-        # form 객체 생성
-        form = CommentModelForm(request.POST)
-        # form valid check
-        if form.is_valid():
-            # author, text 값이 comment 객체에 저장
-            comment = form.save(commit=False)
-            # comment 객체에 매칭되는 post id를 저장
-            comment.post = post
-            # db에 저장
-            comment.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = CommentModelForm()
-    return render(request, 'blog/add_comment_to_post.html', {'form': form})
-
-#Post 삭제
-#@login_required
-def post_remove(request, pk):
-    post = get_object_or_404(Post_board, pk=pk)
-    post.delete()
-    return redirect('board_list', )
+# def add_comment_to_post(request, pk):
+#     post = get_object_or_404(Post_board, pk=pk)
+#     if request.method =='POST':
+#         # form 객체 생성
+#         form = CommentModelForm(request.POST)
+#         # form valid check
+#         if form.is_valid():
+#             # author, text 값이 comment 객체에 저장
+#             comment = form.save(commit=False)
+#             # comment 객체에 매칭되는 post id를 저장
+#             comment.post = post
+#             # db에 저장
+#             comment.save()
+#             return redirect('post_detail', pk=post.pk)
+#     else:
+#         form = CommentModelForm()
+#     return render(request, 'blog/add_comment_to_post.html', {'form': form})
+#
+# #Post 삭제
+# #@login_required
+# def post_remove(request, pk):
+#     post = get_object_or_404(Post_board, pk=pk)
+#     post.delete()
+#     return redirect('board_list', )
 
 
 # post 목록 조회 : 게시일 기준으로 과거에 작성한 글을 필터링하여 정렬하여 글목록 가져오기
