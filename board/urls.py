042a872eff7cf2d3955200982930f0bb1fc3afcf
coconut <wnsspdl@naver.com>
2020-08-12 17:24:05 +0900

    #2 lecture_review urls.py 수정

--- board/urls.py
+++ board/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='main'),
+    # path('', views.index, name='main'),
     path('board', views.board_list, name='board_list'),
 
 ]
