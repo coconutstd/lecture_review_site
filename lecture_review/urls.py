814b393ce523fbcd96b106d8df1f231b399c1bb9
miniongo3o <dlalsdyd094@naver.com>
2020-08-12 10:27:47 +0900

    comment add

--- lecture_review/urls.py
+++ lecture_review/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # main url
     path('admin/', admin.site.urls),
     path('', include('lecture.urls')),
 ]
