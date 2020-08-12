9e2edef3d614a694f500805648311a0a96bad9ec
coconut <wnsspdl@naver.com>
2020-08-12 10:02:29 +0900

    2. Maria DB 연동 코드 추가

--- lecture_review/settings.py
+++ lecture_review/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import pymysql
+pymysql.version_info = (1, 3, 13, "final", 0)
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,9 +78,17 @@ WSGI_APPLICATION = 'lecture_review.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'django_db', # DB명
+         'USER': 'python', # 데이터베이스 계정
+         'PASSWORD':'python', # 계정 비밀번호
+         'HOST':'localhost', # 데이테베이스 IP
+         'PORT':'3306', # 데이터베이스 port
     }
 }
 
