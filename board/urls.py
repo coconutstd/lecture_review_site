08feb400207da2df4661e7088c03a662ce898029
redcarrot01 <redccc9010@gmail.com>
2020-08-14 17:41:07 +0900

    #2 글쓰기와 수정 삭제 오류 해결 못함

--- board/urls.py
+++ board/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('board', views.board_list, name='board_list'),
     # board_detail 페이지 작성하기 # localhost:8000/post/5
     path('board/<int:pk>/', views.board_detail, name='board_detail'),
+    path('board/new/', views.board_new, name='board_new'),
+    path('board/<int:pk>/edit/', views.board_edit, name='board_edit'),
+    path('board/<int:pk>/remove/', views.board_remove, name='board_remove'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,16 +1,56 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 from .models import Post_board, Comment
 from .forms import PostModelForm, PostForm, CommentModelForm
 # Create your views here.
 
+def board_remove(request, pk):
+    postboard = get_object_or_404(Post_board, pk=pk)
+    postboard.delete()
+    return redirect('board_list', )
 
+
+def board_edit(request, pk):
+    # db에서 해당 pk와 매칭되는 Post 객체를 가져온다.
+    postboard = get_object_or_404(Post_board, pk=pk)
+    if request.method == 'POST':
+        # 수정처리
+        form = PostModelForm(request.POST, instance=postboard)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('board_detail', pk=postboard.pk)
+    else:
+        # 수정하기 전에 데이터 읽어옴
+        form = PostModelForm(instance=postboard)
+    return render(request, 'board/board_edit.html', {'form': form})
+
+def board_new(request):
+
+    if request.method =="POST":
+        form =PostForm(request.POST)
+        if form.is_valid():
+            postboard = form.save(commit = False)
+            postboard.author = User.objects.get(username=request.user.username)
+            postboard.published_date=timezone.now()
+            postboard.save()
+            # postboard = Post_board.objects.create(author=request.user,
+            #                            published_date=timezone.now(),
+            #                            title=form.cleaned_data['title'],
+            #                            text=form.cleaned_data['text'])
+            return redirect('board_list', pk=postboard.pk)
+    else:
+        form=PostForm()
+    return render(request, 'board/board_edit.html',{'form':form})
+# 여기 다시
 
 #post 상세 조회
 def board_detail(request, pk):
-    postboard = get_object_or_404(Post_board, pk=pk)
-    return render(request, 'board/board_detail.html', {'postboard':postboard})
+    board_detail = get_object_or_404(Post_board, pk=pk)
+    return render(request, 'board/board_detail.html', {'board_detail':board_detail})
 
 
 
