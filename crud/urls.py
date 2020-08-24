b8f7331b89eeb10b394847341a31743e7159f5e0
redcarrot01 <redccc9010@gmail.com>
2020-08-24 17:21:00 +0900

    #16 강의평 상세 조회와 메인으로 건너가기 완료

--- crud/urls.py
+++ crud/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('crud/<int:lect_id>/', views.eval_list, name = "eval_list"),
     path('crud/<int:lect_id>/write/', views.write, name = "write"),
     path('crud/create/', views.create, name = "create"),
+    # http://127.0.0.1:8000/eval_detail/3 강의평가한 글마다 key 주기 위함
+    path('crud/eval_detail/<int:eval_id>/', views.eval_detail, name="eval_detail")
 ]
