00479dbd570f0a569aea6edebe9a7ffb45c4865a
hair_many_minyong <dlalsdyd094@naver.com>
2020-08-24 16:00:24 +0900

    #5 1. Book/models.py/book_kind 추가 2. 책 종류 별 이미지 추가(static/img) 3. base.html의 url 변경 (book_list->book_type) 4. book_type 템플릿 추가

--- lecture/urls.py
+++ lecture/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('lecture', views.lecture_list, name='lecture_list'),
     path('book', views.book_list, name='book_list'),
     path('signup', views.signup, name='signup'),
+    path('book_type',views.book_type,name='book_type'),
 ]
