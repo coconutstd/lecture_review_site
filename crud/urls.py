ec91d853fdefb392955c1bed43f0695d532a02f8
redcarrot01 <redccc9010@gmail.com>
2020-08-26 12:16:41 +0900

    #16 crud 내용 리뷰중 그리고 추가 기능 고민2

--- crud/urls.py
+++ crud/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('crud', views.crud_lecture_list, name='crud_lecture_list'),
+    # 7. <컨버터형: 전달할 인자 이름> 강의별로 구분할 수 있는 인자 필요하니까
+    # http://127.0.0.1:8000/1/
     path('crud/<int:lect_id>/', views.eval_list, name = "eval_list"),
     path('crud/<int:lect_id>/write/', views.write, name = "write"),
     path('crud/create/', views.create, name = "create"),
